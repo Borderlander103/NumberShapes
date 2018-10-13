07a687db02339fa182a8afac5605576000978aad
Borderlander103 <mailbox@brianrichardsononline.com>
2018-10-13 10:46:52 -0700

    Connect interface items to code

--- app/src/main/java/com/github/borderlander103/numbershapes/MainActivity.java
+++ app/src/main/java/com/github/borderlander103/numbershapes/MainActivity.java
@@ -2,6 +2,9 @@ package com.github.borderlander103.numbershapes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void checkNumberButton(View view) {
+
+        EditText mGuessView = findViewById(R.id.editText);
+
+        String mGuessString = mGuessView.getText().toString();
+
+        Toast.makeText(this, mGuessString, Toast.LENGTH_SHORT).show();
+
+    }
+
 }
